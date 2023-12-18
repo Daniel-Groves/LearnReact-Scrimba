ebb7b39a721eb381eb927481499815b442f0652a
danie <danielgroves2005@gmail.com>
2023-12-18 17:16:19 +0000

    Parent/Child components

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 function Page(){
     return (
         <div>
-            <header>
+            <Header />
+            <Content />
+            <Footer />
+        </div>
+    )
+}
+
+function Header(){
+    return (
+        <header>
                 <nav>
                 <img src="./react-logo.png" alt="React Logo" width="40px"></img>
                 </nav>
-            </header>
+        </header>
+    )
+}
+
+function Footer(){
+    return (
+        <footer>
+                <small>
+                    © 2023 Groves development. All rights reserved.
+                </small>
+        </footer>
+    )
+}
+
+function Content(){
+    return (
+        <div>
             <h1>Reasons I'm excited to learn React</h1>
             <ol>
                 <li>This is reason 1</li>
                 <li>This is reason 2</li>   
                 <li>This is reason 3</li>
             </ol>
-            <footer>
-                <small>
-                    © 2023 Groves development. All rights reserved.
-                </small>
-            </footer>
         </div>
     )
 }
-
 
 // JSX
 ReactDOM.createRoot(document.getElementById("root")).render(<Page />);
