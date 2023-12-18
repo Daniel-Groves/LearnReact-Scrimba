c5aaa9e82feee6069a19b0be87dd834b44e757dd
danie <danielgroves2005@gmail.com>
2023-12-18 17:06:25 +0000

    Custom components part 2

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 function Page(){
     return (
-        <ol>
-            <li>This is reason 1</li>
-            <li>This is reason 2</li>
-            <li>This is reason 3</li>
-        </ol>
+        <div>
+            <header>
+                <nav>
+                <img src="./react-logo.png" alt="React Logo" width="40px"></img>
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>This is reason 1</li>
+                <li>This is reason 2</li>   
+                <li>This is reason 3</li>
+            </ol>
+            <footer>
+                <small>
+                    © 2023 Groves development. All rights reserved.
+                </small>
+            </footer>
+        </div>
     )
 }
 
