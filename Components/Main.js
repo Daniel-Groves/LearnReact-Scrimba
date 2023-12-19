fe48a677e049e9da16b57f86dea1e0b738a45359
danie <danielgroves2005@gmail.com>
2023-12-19 17:25:10 +0000

    First project - styling

--- Components/Main.js
+++ Components/Main.js
@@ -1,6 +1,15 @@
 function Main() {
     return (
-        <h1>Main component goes here</h1>
+        <main className="main">
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--list">
+                <li className="main--list_item">Was first released in 2013</li>
+                <li className="main--list_item">Was originally created by Jordan Walke</li>
+                <li className="main--list_item">Has well over 100K starts on GitHub</li>
+                <li className="main--list_item">Is maintained by Facebook/Meta</li>
+                <li className="main--list_item">Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
 
--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,6 +1,10 @@
 function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav className="navbar">
+            <img src="./Images/react-icon-small.png" className="nav--icon"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
     )
 }
 
