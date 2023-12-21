890db36c5e771b47a7bfb01d05b17350a8a4a88f
danie <danielgroves2005@gmail.com>
2023-12-21 00:09:05 +0000

    getting random image url

--- Project 3 - Meme Generator/Components/Meme.js
+++ Project 3 - Meme Generator/Components/Meme.js
@@ -1,11 +1,16 @@
 function Meme() {
+    function getMeme() {
+        const memesArray = memesData.data.memes;
+        const url = memesArray[Math.floor(Math.random() * memesArray.length)].url;
+        console.log(url)
+    }
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form--input1" placeholder="Shut up" />
                 <input type="text" className="form--input2" placeholder="And take my money" />
-                <button className="form--button">Get a new meme image 🖼️</button>
-            </form>
+                <button className="form--button" onClick={getMeme}>Get a new meme image 🖼️</button>
+            </div>
         </main>
     )
 }
