d2a79dc9e7bf031c17f1541151c9ac64f480efcc
danie <danielgroves2005@gmail.com>
2023-12-21 16:32:52 +0000

    Change to use object in state

--- Project 3 - Meme Generator/Components/Meme.js
+++ Project 3 - Meme Generator/Components/Meme.js
@@ -1,16 +1,25 @@
+
 function Meme() {
+    const [meme, setMeme] = React.useState({
+        randomImage: 'https://i.imgflip.com/1bij.jpg',
+        topText: '',
+        bottomText: ''
+        }
+        );
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData);
     function getMeme() {
-        const memesArray = memesData.data.memes;
+        const memesArray = allMemeImages.data.memes;
         const url = memesArray[Math.floor(Math.random() * memesArray.length)].url;
-        console.log(url)
+        setMeme(meme => ({...meme, randomImage: url}));
     }
     return (
         <main>
             <div className="form">
-                <input type="text" className="form--input1" placeholder="Shut up" />
-                <input type="text" className="form--input2" placeholder="And take my money" />
+                <input type="text" className="form--input" placeholder="Shut up" />
+                <input type="text" className="form--input" placeholder="And take my money" />
                 <button className="form--button" onClick={getMeme}>Get a new meme image 🖼️</button>
             </div>
+            <img src={meme.randomImage} className="meme--image"/>
         </main>
     )
 }
