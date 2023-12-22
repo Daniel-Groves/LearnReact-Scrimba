09fcb100e9c1d9b279673808ba82530cd6065a0d
danie <danielgroves2005@gmail.com>
2023-12-22 16:01:37 +0000

    More conditional rendering practice

--- Props practice/Joke.js
+++ Props practice/Joke.js
@@ -1,8 +1,13 @@
 function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false)
+    function toggleIsShown() {
+        setIsShown(isShown => !isShown);
+    }
     return (
         <div className="joke">
-            <h1>{props.setup}</h1>
-            <h2>{props.punchline}</h2>
+            {props.setup && <h1>{props.setup}</h1>}
+            {isShown && <h2>{props.punchline}</h2>}
+            <button onClick={toggleIsShown}>Toggle isShown</button>
             <hr></hr>
         </div>
     )
